dacad66769907d1018a09e7e92ac43790d5a1da1
felicityzhang <xz1574@nyu.edu>
2019-04-18 12:04:51 -0400

    add conctact

--- chumleys_app/src/App.js
+++ chumleys_app/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header'
 import './App.css';
-
+import Contact from './Contact'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-
+        <Contact />
       </div>
     );
   }
--- chumleys_app/src/Contact.js
+++ chumleys_app/src/Contact.js
@@ -6,12 +6,23 @@ export default class Contact extends Component {
         return (
             <div className='contact-main'>
                 <div className='Rectangle-Copy'>
+                    <div className="Text-Box">
+                        <div className='Hours-Location'>Hours & Location</div>
+                        <div className='Dinner-Monday-Satu'>
+                            <div classNane='Dinner-Monday-Satu text-style-1 '>Dinner</div>
+                            <p>Monday - Saturday</p>
+                            <p>5:30pm - 12am</p>
+                            <div classNane='Dinner-Monday-Satu text-style-1 '>Sunday Brunch</div>
+                            <p>11:30pm - 3:30pm</p>
+                        </div>
+                        <div><img className='Rectangle-Copy-2' src='./images/Map.png' /></div>
+                    </div>
                 </div>
-                <div className='Booth'>
-                    <img src=''>
+                <div>
+                    <img className='Booth' alt='boothImage' src='./images/Booth.png' />
                 </div>
-                </div>
-                )
-            }
-        }
-        
+            </div>
+        )
+    }
+}
+
