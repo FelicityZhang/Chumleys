4df721c8bb629e89262fa199c9a0080d587098f6
imanipaul <imanipaul@wustl.edu>
2019-04-18 16:42:46 -0400

    Add pictures

---
+++ chumleys_app/src/Components/Parallax.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Parallax() {
+    return (
+        <div className='parallax'>
+            <div className='rellax'>
+                <img src={} />
+            </div>
+            <div className='rellax'>
+                <img src={} />
+            </div>
+            <div className='rellax'>
+                <img src={} />
+            </div>
+            <div className='rellax'>
+                <img src={} />
+            </div>
+        </div>
+    )
+}
