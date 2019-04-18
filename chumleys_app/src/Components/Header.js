02f92df23a0f9513d3ba97e7fc45e27abfb89075
imanipaul <imanipaul@wustl.edu>
2019-04-18 11:43:59 -0400

    Style chumleys logo and year

--- chumleys_app/src/Components/Header.js
+++ chumleys_app/src/Components/Header.js
@@ -1,12 +1,10 @@
 import React from 'react'
-// import backgroundImage from '../images/View_of_Bar.jpg'
 import Navbar from './Navbar'
 
 export default function Header(props) {
     return (
         <div className='View_of_Bar'>
             <Navbar />
-            {/* <img src={backgroundImage} /> */}
         </div>
     )
 }
--- chumleys_app/src/Components/Navbar.js
+++ chumleys_app/src/Components/Navbar.js
@@ -4,7 +4,10 @@ import logo from '../images/logo.png'
 export default function Navbar(props) {
     return (
         <div className='navbar'>
-            <img src={logo} className='logo' />
+            <div className='nav-img-year'>
+                <img src={logo} className='logo' />
+                <p className='established'>Established 1922</p>
+            </div>
             <a href="#">Menu</a>
             <a href="#">Our Story</a>
             <a href="#">Hours &amp; Location</a>
