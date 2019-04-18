873169bb80acf9b45a8dfc31c64ee3afdc08b882
imanipaul <imanipaul@wustl.edu>
2019-04-18 14:59:24 -0400

    create our story component

--- chumleys_app/src/App.js
+++ chumleys_app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Components/Header'
 import ChefBlock from './Components/ChefBlock'
+import OurStory from './Components/OurStory'
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <ChefBlock />
+        <OurStory />
 
 
       </div>
