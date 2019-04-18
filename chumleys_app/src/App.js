e8f3b726ded63942eb3aeec1caccea1162b420eb
Ed Roesch <edroesch@gmail.com>
2019-04-18 17:22:30 -0400

    everything for contactpage component

--- chumleys_app/src/App.js
+++ chumleys_app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Components/Header'
 import ChefBlock from './Components/ChefBlock'
+import ContactPage from './Components/ContactPage'
 import './App.css';
 
 class App extends Component {
@@ -9,7 +10,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <ChefBlock />
-
+        <ContactPage />
 
       </div>
     );
