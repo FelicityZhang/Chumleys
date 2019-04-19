b021db9fff219bed9103adfc3e8dd9fd73e65004
felicityzhang <xz1574@nyu.edu>
2019-04-19 15:04:59 -0400

    display quote

--- chumleys_app/src/Components/HemQuote.js
+++ chumleys_app/src/Components/HemQuote.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
-export default function HemQuote(props) {
+export default function HemQuote( props ) {
     return (
         <div className='hem-quote'>
-            <div className='hem-quote-content'>
-                Drinking wine was not a snobbism nor a sign of sophistication nor a cult; it was as natural as eating and to me, as necessary.
-        </div>
-            <div className='hem-author'>
-                Ernest Hemingway
-        </div>
+            <div className="displayQuote">
+
+                <div className='hem-quote-content'>
+                    Drinking wine was not a snobbism nor a sign of sophistication nor a cult; it was as natural as eating and to me, as necessary.
+                </div>
+                <div className='hem-author'>
+                    Ernest Hemingway
+                 </div>
+            </div>
         </div>
     )
 }
