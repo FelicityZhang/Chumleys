faa974f512bcbeada69814752551620ab8b76b6b
felicityzhang <xz1574@nyu.edu>
2019-04-19 12:27:13 -0400

    fixed fireplace

--- chumleys_app/src/Components/ContactPage.js
+++ chumleys_app/src/Components/ContactPage.js
@@ -2,22 +2,14 @@ import React from 'react'
 import Fireplace_Restaurant from '../images/Fireplace.png'
 import Mobile_Fireplace from '../images/Mobile_Fireplace.png'
 
-export default function ContactPage(props) {
+export default function ContactPage( props ) {
     return (
         <div className='contactpage-block'>
-            {/* <div className='contactpage-image'> */}
+            {/* <div className='contactpage-image'> */ }
             <div>
-            <img className='mobile-fireplace' src={Mobile_Fireplace} />
+                <img className='mobile-fireplace' src={ Mobile_Fireplace } />
 
-            <img className='contactpage-image' src={Fireplace_Restaurant} />
-import Fireplace from '../images/Fireplace.png'
-
-
-export default function ContactPage(props) {
-    return (
-        <div className='contactpage-block' id='contactpage'>
-            <div className='contactpage-image'>
-                <img src={Fireplace} />
+                <img className='contactpage-image' src={ Fireplace_Restaurant } />
             </div>
 
             <div className='contact-holder'>
@@ -25,31 +17,21 @@ export default function ContactPage(props) {
                     <p className='contact-header'>Contact</p>
 
                     <div className='contact-content'>
-                    <div className='desktop-contact-text'>
-                        <span className='media-contact'>For press inquiries, please email jrosen@chumleysnewyork.com
+                        <div className='desktop-contact-text'>
+                            <span className='media-contact'>For press inquiries, please email jrosen@chumleysnewyork.com
                     </span>
-                        <p className='allelse-contact'>For anything else, contact us by Phone <span className='Phone'>(212) 675-2081</span> <br></br> or fill out the form below.
+                            <p className='allelse-contact'>For anything else, contact us by Phone <span className='Phone'>(212) 675-2081</span> <br></br> or fill out the form below.
                     </p>
-                    </div>
-                    <div className='mobile-contact-text'>
-                        <span className='media-contact'>Reservations can be arranged by phone Monday through Saturday from 10:00am to 10:00pm, or through Resy. </span> 
-                        <br></br>
-                        <br></br>
-                        <span className='media-contact'> While a credit card is not required for reservations under six guests, we ask that you confirm at least 24 hours before your reservation. </span>
-                    
-                        <p className='allelse-contact'>If your plans should change, please reach out to us. <br></br><br></br><span className='Phone'>(212) 675-2081</span> <br></br>
-                    </p>
-                    </div>
-                    <div className='input1'>
-                        <div className='name'>Name</div>
-                    </div>
-                    <div className='input1'>
-                        <div className='name'>Email</div>
-                    </div>
-                    <div className='input2'>
-                        <div className='name'>Message</div>
-                    </div>
-                    <button className='contact-button'>SEND</button>
+                        </div>
+                        <div className='mobile-contact-text'>
+                            <span className='media-contact'>Reservations can be arranged by phone Monday through Saturday from 10:00am to 10:00pm, or through Resy. </span>
+                            <br></br>
+                            <br></br>
+                            <span className='media-contact'> While a credit card is not required for reservations under six guests, we ask that you confirm at least 24 hours before your reservation. </span>
+
+                            <p className='allelse-contact'>If your plans should change, please reach out to us. <br></br><br></br><span className='Phone'>(212) 675-2081</span> <br></br>
+                            </p>
+                        </div>
                         <div className='input1'>
                             <div className='name'>Name</div>
                         </div>
